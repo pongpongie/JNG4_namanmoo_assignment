31b73b8b254c084783eb12a57a2b2635161521ce
pongpongie <arevolvera@gmail.com>
2024-04-11 00:47:38 +0900

    feat: implement login and signup components
    App.js
    - Deleted BoardContent()
    - Implemented SignUp() and Login()
    
    App.css
    - Add styles to login,signup and changeMode classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import { useState } from "react";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function Board() {
+function Board(props) {
+  console.log(props);
   return (
     <div className="board">
       <BoardLeft />
-      <BoardRight />
+      <BoardRight mode={props.mode} onChange={props.onChange} />
     </div>
   );
 }
@@ -12,45 +15,75 @@ function Board() {
 function BoardLeft() {
   return (
     <div className="board-left">
-      <BoardContent />
-    </div>
-  );
-}
-
-function BoardRight() {
-  return (
-    <div className="board-right">
-      <Login />
-      <SubmitArea />
-    </div>
-  );
-}
-
-function BoardContent() {
-  return (
-    <div className="boardContent">
       <h1>Jungle Board</h1>
     </div>
   );
 }
 
-function Login() {
+function BoardRight(props) {
+  console.log(props);
+  if (props.mode === "LOGIN") {
+    return (
+      <div className="board-right">
+        <Login mode={props.mode} onChange={props.onChange} />
+        {/* <Submit mode={props.mode} /> */}
+      </div>
+    );
+  } else if (props.mode === "SIGNUP") {
+    return (
+      <div className="board-right">
+        <SignUp mode={props.mode} onChange={props.onChange} />
+        {/* <Submit /> */}
+      </div>
+    );
+  }
+}
+
+function Login(props) {
   return (
     <div className="login">
       <input type="text" placeholder="아이디" />
       <input type="text" placeholder="패스워드" />
+      <button>로그인</button>
+      <div className="changeMode">
+        <p>계정이 없으신가요?</p>
+        <button
+          onClick={() => {
+            props.onChange("SIGNUP");
+          }}
+        >
+          회원가입
+        </button>
+      </div>
     </div>
   );
 }
 
-function SubmitArea() {
-  return <a href="/submit">제출하기</a>;
+function SignUp(props) {
+  return (
+    <div className="signUp">
+      <input type="text" placeholder="아이디" />
+      <input type="text" placeholder="패스워드" />
+      <input type="text" placeholder="이메일" />
+      <div className="changeMode">
+        <p>로그인으로 돌아가기</p>
+        <button
+          onClick={() => {
+            props.onChange("LOGIN");
+          }}
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
 }
 
 function App() {
+  const [mode, setMode] = useState("LOGIN");
   return (
     <div className="App">
-      <Board />
+      <Board mode={mode} onChange={setMode} />
     </div>
   );
 }
